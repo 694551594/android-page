7ea36c7d0d8519fdcb702f2b914e96ba96041f6f
yanghuiqiang <694551594@qq,com>
2016-10-11 20:54:53 +0800

    重构

---
+++ library/src/main/java/cn/yhq/page/http/RetrofitPageDataActivity.java
@@ -0,0 +1,25 @@
+package cn.yhq.page.http;
+
+import cn.yhq.page.core.IPageRequester;
+import cn.yhq.page.core.PageAction;
+import cn.yhq.page.ui.PageDataActivity;
+
+public abstract class RetrofitPageDataActivity<T, I> extends PageDataActivity<T, I>
+        implements
+        RetrofitPageRequester.IPageRequestExecutor<T, I> {
+
+    @Override
+    public void onPageLoadComplete(PageAction pageAction, boolean isFromCache, boolean isSuccess) {
+        super.onPageLoadComplete(pageAction, isFromCache, isSuccess);
+        if (pageAction == PageAction.INIT && isFromCache) {
+            this.refreshPageData();
+        }
+    }
+
+    @Override
+    public IPageRequester<T, I> getPageRequester() {
+        return new RetrofitPageRequester<>(this, this);
+    }
+
+
+}
---
+++ library/src/main/java/cn/yhq/page/http/RetrofitPageDataFragment.java
@@ -0,0 +1,30 @@
+package cn.yhq.page.http;
+
+import cn.yhq.page.core.IPageRequester;
+import cn.yhq.page.core.PageAction;
+import cn.yhq.page.ui.PageDataFragment;
+
+/**
+ * 使用retrofit和okhttp封装的分页列表请求处理框架
+ *
+ * @param <T>
+ * @param <I>
+ * @author Yanghuiqiang 2015-10-10
+ */
+public abstract class RetrofitPageDataFragment<T, I> extends PageDataFragment<T, I>
+        implements
+        RetrofitPageRequester.IPageRequestExecutor<T, I> {
+
+    @Override
+    public void onPageLoadComplete(PageAction pageAction, boolean isFromCache, boolean isSuccess) {
+        super.onPageLoadComplete(pageAction, isFromCache, isSuccess);
+        if (pageAction == PageAction.INIT && isFromCache) {
+            this.refreshPageData();
+        }
+    }
+
+    @Override
+    public IPageRequester<T, I> getPageRequester() {
+        return new RetrofitPageRequester<>(this.getContext(), this);
+    }
+}
